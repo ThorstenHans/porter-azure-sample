60484e230e7c1296f7f89c89a4bca60c299bdbcd
Thorsten Hans <thorsten.hans@gmail.com>
2022-07-01 12:12:28 +0200

    fix: use proper name for aks
    Signed-off-by: Thorsten Hans <thorsten.hans@gmail.com>

--- bundle/terraform/main.tf
+++ bundle/terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  name                = "aks-porter-sample-202"
+  name                = "aks-porter-sample-2022"
   dns_prefix          = "aks-porter"
   identity {
     type = "SystemAssigned"
